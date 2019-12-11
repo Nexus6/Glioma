9d83417e3803eb95202c9ecfc5721f2b10e0991b
Eric Anderson <eric@ultracode.com>
2019-12-10 22:18:27 -0800

    Cleanup. Set version to 1.0.2, indicate stable status, minor license tweak, clarify python versions supported.

--- setup.py
+++ setup.py
@@ -11,9 +11,9 @@ long_description=""
 
 setup(
     name='Glioma',
-    version='0.8.7',
+    version='1.0.2',
 
-    description="A small library of immutable collections classes, inspired by those in the Scala standard library, written in Coconut.",
+    description="A small library of immutable collections classes, inspired by those in the Scala standard library.",
     long_description='',
 
     # The project's main homepage.
@@ -24,31 +24,33 @@ setup(
     author_email='ultracode@gmail.com',
 
     # Choose your license
-    license='Apache 2.03',
+    license='Apache 2.0',
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 4 - Beta',
-
-        # Indicate who your project is intended for
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries :: Python Modules',
-
-        # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: Apache 2.03',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
+        'License :: OSI Approved :: Apache Software License',
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
     ],
 
     # What does your project relate to?
-    keywords='Scala functional immutable collections Coconut',
+    keywords='Scala functional immutable collections',
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
